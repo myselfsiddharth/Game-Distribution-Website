059938eb214d7b99f44ea740923b031fca0274ca
sidd09k <99870733+sidd09k@users.noreply.github.com>
2022-10-09 13:29:52 -0700

    Update urls.py

--- Game/urls.py
+++ Game/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('/', views.index, name='index'),
     path('games', views.games, name='games'),
     path('test', views.test, name='test'),
     path('contact', views.contact, name="contact"),
